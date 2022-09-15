8d909947c2349fc694eeef0a23d27501aa5899c5
vanny <vdevelopper@gmail.com>
2022-09-15 08:48:48 +0200

    conflits avec Texfield

--- src/screens/welcome/LoginScreen.jsx
+++ src/screens/welcome/LoginScreen.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View, useWindowDimensions } from "react-native";
-
+// import { TextField } from "react-native-material-textfield";
 
 export default function LoginScreen(){
         const { height } = useWindowDimensions()
         return(
                 <View style={{marginTop:50}}>
                         <Text>Login</Text>
+                        // <TextField/>
                 </View>
         )
 }
