99111b185c9be7e9f5aa60117f0269e60f5f9ce2
claudine101 <ndayisabaclaudine01@gmail.com>
2022-09-21 16:29:30 +0200

    AJOUT  LES IMAGES ROULANT

--- App.js
+++ App.js
@@ -7,11 +7,14 @@ import { Provider } from 'react-redux';
 import {store} from './src/store'
 import AchatProduitsScreens from './src/screens/e-commerce/AchatProduitsScreens';
 import CommandeEmiseScreen from './src/screens/e-commerce/CommandeEmiseScreen';
+import Carousel from './src/screens/homeDrawer/carousel';
+import HomeScreen from './src/screens/home/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-    <AppContainer/>
+    <HomeScreen/>
+    {/* // <AppContainer/> */}
     </Provider>
 
   )
