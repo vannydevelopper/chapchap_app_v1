05b1cdc15d8b19d93ce2c2d0f546b3ac183265ed
dukizwe <mbagapro@gmail.com>
2022-09-24 23:59:20 +0200

    feat(app): add modalize on services

--- src/components/app/ServicesCategories.jsx
+++ src/components/app/ServicesCategories.jsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { StyleSheet, View, Text, Image, useWindowDimensions, ImageBackground, Vibration, TouchableNativeFeedback } from 'react-native'
+import { Modalize } from 'react-native-modalize'
 import { COLORS } from '../../styles/COLORS'
 
 const SERVICES = [{
@@ -30,47 +31,58 @@ const SERVICES = [{
           icon:  require('../../../assets/images/pub-icon.png')
 }]
 export default function ServicesCategories() {
-          const { width } = useWindowDimensions()
+          const { width, height } = useWindowDimensions()
           const SERVICE_MARGIN = 40
           const SERVICE_WIDTH = (width / 2)
+          const modalizeRef = useRef(null)
 
           const navigation = useNavigation()
+          useEffect(() => {
+                    modalizeRef.current?.open()
+          }, [modalizeRef])
           return (
-                    <View style={styles.servicesContainer}>
-                              <View style={styles.handle} />
-                              <Text style={styles.title}>Catégories de service</Text>
-                              <View style={styles.services}>
-                                        {SERVICES.map((service, index) => {
-                                                  return (
-                                                            <View style={[styles.serviceContainer, { width: SERVICE_WIDTH, height: SERVICE_WIDTH }]} key={index.toString()}>
-                                                                      <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple("#C4C4C4")} useForeground onPress={() => navigation.navigate(service.route)}>
-                                                                      <View style={[styles.service]}>
-                                                                                <ImageBackground source={service.imageBg} style={[styles.serviceBackgound]} borderRadius={10} resizeMode='cover' imageStyle={{ opacity: 0.8 }}>
-                                                                                          <View style={{ position: 'absolute', width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.3)", borderRadius: 10}} />
-                                                                                          <View style={styles.serviceIcon}>
-                                                                                                    <Image source={service.icon} style={styles.serviceIconImage} />
-                                                                                          </View>
-                                                                                          <Text style={styles.serviceName}>{ service.title }</Text>
-                                                                                </ImageBackground>
+                    <Modalize
+                              ref={modalizeRef}
+                              alwaysOpen={(height/2) + 150 }
+                              modalStyle={styles.servicesContainer}
+                              handleStyle={{ backgroundColor: '#D6ECEC' }}
+                              handlePosition="inside"
+                              withOverlay
+                              dragToss={1}
+                    >
+                              <View style={{}}>
+                                        {/* <View style={styles.handle} /> */}
+                                        <Text style={styles.title}>Catégories de service</Text>
+                                        <View style={styles.services}>
+                                                  {SERVICES.map((service, index) => {
+                                                            return (
+                                                                      <View style={[styles.serviceContainer, { width: SERVICE_WIDTH, height: SERVICE_WIDTH }]} key={index.toString()}>
+                                                                                <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple("#C4C4C4")} useForeground onPress={() => navigation.navigate(service.route)}>
+                                                                                <View style={[styles.service]}>
+                                                                                          <ImageBackground source={service.imageBg} style={[styles.serviceBackgound]} borderRadius={10} resizeMode='cover' imageStyle={{ opacity: 0.8 }}>
+                                                                                                    <View style={{ position: 'absolute', width: "100%", height: "100%", backgroundColor: "rgba(0, 0, 0, 0.3)", borderRadius: 10}} />
+                                                                                                    <View style={styles.serviceIcon}>
+                                                                                                              <Image source={service.icon} style={styles.serviceIconImage} />
+                                                                                                    </View>
+                                                                                                    <Text style={styles.serviceName}>{ service.title }</Text>
+                                                                                          </ImageBackground>
+                                                                                </View>
+                                                                      </TouchableNativeFeedback>
                                                                       </View>
-                                                            </TouchableNativeFeedback>
-                                                            </View>
-                                                  )
-                                        })}
+                                                            )
+                                                  })}
+                                        </View >
                               </View >
-                    </View >
+                    </Modalize>
           )
 }
 
 const styles = StyleSheet.create({
           servicesContainer: {
-                    elevation: 5,
-                    borderTopRightRadius: 40,
-                    borderTopLeftRadius: 40,
-                    shadowColor: '#C3BABA',
-                    flex: 1,
-                    paddingVertical: 20,
-                    marginTop: 10
+                    elevation: 10,
+                    shadowColor: '#000',
+                    borderTopRightRadius: 30,
+                    borderTopLeftRadius: 30,
           },
           handle: {
                     height: 5,
@@ -84,7 +96,8 @@ const styles = StyleSheet.create({
                     fontWeight: "bold",
                     marginBottom: 10,
                     fontSize: 20,
-                    textAlign: 'center'
+                    textAlign: 'center',
+                    marginTop: 20
           },
           services: {
                     flexDirection: 'row',
