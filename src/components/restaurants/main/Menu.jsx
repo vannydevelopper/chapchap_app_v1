d5ecae186659725a4e666c232e740d560e6722e8
claudine101 <ndayisabaclaudine01@gmail.com>
2022-09-29 14:47:38 +0200

    resolution des conflits

--- src/components/restaurants/main/Menu.jsx
+++ src/components/restaurants/main/Menu.jsx
@@ -12,15 +12,51 @@ import { useSelector } from 'react-redux';
 import { ecommerceProductSelector } from '../../../store/selectors/ecommerceCartSelectors';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Menu({ product, index, totalLength, fixMargins = false }) {
-       
+export default function Menu({ menu, index, totalLength, fixMargins = false }) {
+  const navigation = useNavigation()
+  const { width } = useWindowDimensions()
+  const PRODUCT_MARGIN = 10
+  const PRODUCT_WIDTH = (width / 2) - PRODUCT_MARGIN - 10
+  const PRODUCT_HEIGHT = 270
+  const additionStyles = {
+            width: PRODUCT_WIDTH,
+            height: PRODUCT_HEIGHT,
+            marginLeft: index > 0 ? PRODUCT_MARGIN : (fixMargins ? PRODUCT_MARGIN : 0),
+            marginRight: index == totalLength-1 ? PRODUCT_MARGIN : (fixMargins ? 0 : 0)
+  }
+  
+  const modalizeRef = useRef(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [loadingForm, setLoadingForm] = useState(true)
+
+  const onCartPress = () => {
+            setIsOpen(true)
+            modalizeRef.current?.open()
+  }
+
+  const onCloseAddToCart = () => {
+            modalizeRef.current?.close()
+  }
+
+  const productInCart = useSelector(ecommerceProductSelector(menu.ID_RESTAURANT_MENU))
+
+  useEffect(() => {
+            if(isOpen) {
+                      const timer = setTimeout(() => {
+                                setLoadingForm(false)
+                      })
+                      return () => {
+                                clearTimeout(timer)
+                      }
+            }
+  }, [isOpen])
 
           return (
             <View key={index} style={[styles.product, additionStyles]}>
-                              <TouchableOpacity onPress={()=>navigation.push('ProductDetailsScreen', {product:product})} style={styles.imageCard}>
-                                        <Image source={{ uri: product.IMAGE}} style={styles.image} />
+                              <TouchableOpacity onPress={()=>navigation.push('ProductDetailsScreen', {product:menu})} style={styles.imageCard}>
+                                        <Image source={{ uri: menu.IMAGE}} style={styles.image} />
                               </TouchableOpacity>
-                              {/* <View style={{ flexDirection: "row" }}>
+                              <View style={{ flexDirection: "row" }}>
                                         <View style={styles.cardLike}>
                                                   <AntDesign name="hearto" size={24} color="#F29558" />
                                         </View>
@@ -32,14 +68,14 @@ export default function Menu({ product, index, totalLength, fixMargins = false }
                                                   </View> : null}
                                                   </>
                                         </TouchableOpacity>
-                              </View> */}
+                              </View>
                               <View style={styles.productNames}>
                                         <Text numberOfLines={2} style={styles.productName}>
-                                                  {product.NOM} ·
-                                                  <Text numberOfLines={2} style={styles.productName}> {product.produit_partenaire.NOM}</Text>
+                                                  {menu.NOM} ·
+                                                  <Text numberOfLines={2} style={styles.productName}> {menu.NOM_SOUS_CATEGORIE}</Text>
                                         </Text>
                               </View>
-                              {/* {product.produit_partenaire.PRIX ? <Text style={{ color: "#F29558", fontWeight: "bold" }}>{product.produit_partenaire.PRIX.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") } Fbu</Text> : null}
+                              {menu.MONTANT ? <Text style={{ color: "#F29558", fontWeight: "bold" }}>{menu.MONTANT.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") } Fbu</Text> : null}
                               <Portal>
                                         <GestureHandlerRootView style={{ height: isOpen ? '100%' : 0, opacity: isOpen ? 1 : 0, backgroundColor: 'rgba(0, 0, 0, 0)', position: 'absolute', width: '100%', zIndex: 1 }}>
                                                   <Modalize
@@ -60,10 +96,10 @@ export default function Menu({ product, index, totalLength, fixMargins = false }
                                                                       setLoadingForm(true)
                                                             }}
                                                   >
-                                                            <AddCart product={product} loadingForm={loadingForm} onClose={onCloseAddToCart} />
+                                                            <AddCart product={menu} loadingForm={loadingForm} onClose={onCloseAddToCart} />
                                                   </Modalize>
                                         </GestureHandlerRootView>
-                              </Portal> */}
+                              </Portal>
                     </View>
                     
           )
