2cd9c8cb8d87f23cc6de29ca1a739592b4a3627d
claudine101 <ndayisabaclaudine01@gmail.com>
2022-11-17 11:04:31 +0200

    correction sur  shoppind

--- src/components/ecommerce/main/ProductPartenaire.jsx
+++ src/components/ecommerce/main/ProductPartenaire.jsx
@@ -14,115 +14,151 @@ import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import fetchApi from '../../../helpers/fetchApi';
 import { useCallback } from 'react';
 
-export default function ProductPartenaire({ product, index, totalLength, fixMargins = false ,onRemove }) {
-        const navigation = useNavigation()
-          const { width } = useWindowDimensions()
-          const PRODUCT_MARGIN = 10
-          const PRODUCT_WIDTH = (width / 2) - PRODUCT_MARGIN - 10
-          const PRODUCT_HEIGHT = 270
-          const additionStyles = {
-                    width: PRODUCT_WIDTH,
-                    height: PRODUCT_HEIGHT,
-                    marginLeft: index > 0 ? PRODUCT_MARGIN : (fixMargins ? PRODUCT_MARGIN : 0),
-                    marginRight: index == totalLength-1 ? PRODUCT_MARGIN : (fixMargins ? 0 : 0)
-          }
+export default function ProductPartenaire({ product, index, totalLength, fixMargins = false, onRemove }) {
+  const navigation = useNavigation()
+  const { width } = useWindowDimensions()
+  const PRODUCT_MARGIN = 10
+  const PRODUCT_WIDTH = (width / 2) - PRODUCT_MARGIN - 10
+  const PRODUCT_HEIGHT = 270
+  const additionStyles = {
+    width: PRODUCT_WIDTH,
+    height: PRODUCT_HEIGHT,
+    marginLeft: index > 0 ? PRODUCT_MARGIN : (fixMargins ? PRODUCT_MARGIN : 0),
+    marginRight: index == totalLength - 1 ? PRODUCT_MARGIN : (fixMargins ? 0 : 0)
+  }
 
-          const modalizeRef = useRef(null)
-          const [isOpen, setIsOpen] = useState(false)
-          const [loadingForm, setLoadingForm] = useState(true)
-          const [wishlist, setWishlist] = useState(false)
-          const fecthWishlist = async () => {
-            try {
-              const wishliste = await fetchApi(`/wishlist/verification/${product.produit.ID_PRODUIT_PARTENAIRE}`, {
-                method: "GET",
-                headers: { "Content-Type": "application/json" },
-              })
-              // console.log(wishliste.result)
-              if (wishliste.result) {
-                setWishlist(true)
-              }
-        
-            }
-            catch (error) {
-              console.log(error)
-            }
-          }
-          useFocusEffect(useCallback(() => {
-            fecthWishlist()
-          }, []))
-          const Addishlist = async (id) => {
-            //  console.log(id)
-            if (wishlist) {
-              try {
-        
-                const newWishlist = await fetchApi(`/wishlist/suppression/${id}`, {
-                  method: "DELETE",
-                })
-                if (onRemove) {
-                  onRemove(id)
-                }
-        
-                setWishlist(false)
-        
-              } catch (error) {
-                console.log(error)
-              }
-        
-            }
-        
-            else {
-              try {
-                const form = new FormData()
-                // form.append("ID_PRODUIT", id)
-                //  console.log(id)
-                const newWishlist = await fetchApi('/wishlist', {
-                  method: 'POST',
-                  body: JSON.stringify({
-                    ID_PRODUIT_PARTENAIRE: id,
-        
-                  }),
-                  headers: { "Content-Type": "application/json" },
-                })
-        
-                setWishlist(true)
-        
-        
-              } catch (error) {
-                console.log(error)
-              }
-        
-            }
-        
-          }
-          const onCartPress = () => {
-                    setIsOpen(true)
-                    modalizeRef.current?.open()
-          }
+  const modalizeRef = useRef(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [loadingForm, setLoadingForm] = useState(true)
+  const [wishlist, setWishlist] = useState(false)
+  const [colors, SetColors] = useState([])
+  const [SIZES, setSIZES] = useState([])
+  const fecthSizes = async () => {
+    try {
+      const sizes = await fetchApi(`/products/size/${product.produit.ID_PRODUIT_PARTENAIRE}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
 
-          const onCloseAddToCart = () => {
-                    modalizeRef.current?.close()
-          }
+      })
+      setSIZES(sizes.result)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
 
-          const productInCart = useSelector(ecommerceProductSelector(product.ID_PRODUIT_PARTENAIRE))
+  useFocusEffect(useCallback(() => {
+    fecthSizes()
+  }, []))
 
-          useEffect(() => {
-                    if(isOpen) {
-                              const timer = setTimeout(() => {
-                                        setLoadingForm(false)
-                              })
-                              return () => {
-                                        clearTimeout(timer)
-                              }
-                    }
-          }, [isOpen])
+  const onSizePress = async (size) => {
 
-          return (
-                    <View key={index} style={[styles.product, additionStyles]}>
-                              <TouchableOpacity onPress={()=>navigation.push('ProductDetailsScreen', {product:product})} style={styles.imageCard}>
-                                        <Image source={{ uri: product.produit_partenaire.IMAGE_1 }} style={styles.image} />
-                              </TouchableOpacity>
-                              <View style={{ flexDirection: "row" }}>
-                              <TouchableOpacity
+    try {
+      // setLoadingSubCategories(true)
+      if (size?.id) {
+        const color = await fetchApi(`/products/color/${product.produit.ID_PRODUIT_PARTENAIRE}/${size?.id}`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        })
+        SetColors(color.result)
+
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const fecthWishlist = async () => {
+    try {
+      const wishliste = await fetchApi(`/wishlist/verification/${product.produit.ID_PRODUIT_PARTENAIRE}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      })
+      // console.log(wishliste.result)
+      if (wishliste.result) {
+        setWishlist(true)
+      }
+
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+  useFocusEffect(useCallback(() => {
+    fecthWishlist()
+  }, []))
+  const Addishlist = async (id) => {
+    //  console.log(id)
+    if (wishlist) {
+      try {
+
+        const newWishlist = await fetchApi(`/wishlist/suppression/${id}`, {
+          method: "DELETE",
+        })
+        if (onRemove) {
+          onRemove(id)
+        }
+
+        setWishlist(false)
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+    else {
+      try {
+        const form = new FormData()
+        // form.append("ID_PRODUIT", id)
+        //  console.log(id)
+        const newWishlist = await fetchApi('/wishlist', {
+          method: 'POST',
+          body: JSON.stringify({
+            ID_PRODUIT_PARTENAIRE: id,
+
+          }),
+          headers: { "Content-Type": "application/json" },
+        })
+
+        setWishlist(true)
+
+
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+  }
+  const onCartPress = () => {
+    setIsOpen(true)
+    modalizeRef.current?.open()
+  }
+
+  const onCloseAddToCart = () => {
+    modalizeRef.current?.close()
+  }
+
+  const productInCart = useSelector(ecommerceProductSelector(product.ID_PRODUIT_PARTENAIRE))
+
+  useEffect(() => {
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        setLoadingForm(false)
+      })
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+  }, [isOpen])
+
+  return (
+    <View key={index} style={[styles.product, additionStyles]}>
+      <TouchableOpacity onPress={() => navigation.push('ProductDetailsScreen', { product: product })} style={styles.imageCard}>
+        <Image source={{ uri: product.produit_partenaire.IMAGE_1 }} style={styles.image} />
+      </TouchableOpacity>
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity
           onPress={() => {
             Addishlist(product.produit.ID_PRODUIT_PARTENAIRE)
             setWishlist(true)
@@ -133,105 +169,106 @@ export default function ProductPartenaire({ product, index, totalLength, fixMarg
           </View>
         </TouchableOpacity>
 
-                                        <TouchableOpacity style={styles.cartBtn} onPress={onCartPress}>
-                                                  <>
-                                                  <AntDesign name="shoppingcart" size={24} color="#F29558" />
-                                                  {productInCart ? <View style={styles.badge}>
-                                                            <Text style={styles.badgeText} numberOfLines={1}>{ productInCart.QUANTITE }</Text>
-                                                  </View> : null}
-                                                  </>
-                                        </TouchableOpacity>
-                              </View>
-                              <View style={styles.productNames}>
-                                        <Text numberOfLines={2} style={styles.productName}>
-                                                  
-                                                  <Text numberOfLines={2} style={styles.productName}> {product.produit_partenaire.NOM}</Text>
-                                        </Text>
-                              </View>
-                              {product.produit_partenaire.PRIX ? <Text style={{ color: "#F29558", fontWeight: "bold" }}>{product.produit_partenaire.PRIX.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") } Fbu</Text> : null}
-                              <Portal>
-                                        <GestureHandlerRootView style={{ height: isOpen ? '100%' : 0, opacity: isOpen ? 1 : 0, backgroundColor: 'rgba(0, 0, 0, 0)', position: 'absolute', width: '100%', zIndex: 1 }}>
-                                                  <Modalize
-                                                            ref={modalizeRef}
-                                                            adjustToContentHeight
-                                                            handlePosition='inside'
-                                                            modalStyle={{
-                                                                      borderTopRightRadius: 25,
-                                                                      borderTopLeftRadius: 25,
-                                                                      paddingVertical: 20
-                                                            }}
-                                                            handleStyle={{ marginTop: 10 }}
-                                                            scrollViewProps={{
-                                                                      keyboardShouldPersistTaps: "handled"
-                                                            }}
-                                                            onClosed={() => {
-                                                                      setIsOpen(false)
-                                                                      setLoadingForm(true)
-                                                            }}
-                                                  >
-                                                            <AddCart product={product} loadingForm={loadingForm} onClose={onCloseAddToCart} />
-                                                  </Modalize>
-                                        </GestureHandlerRootView>
-                              </Portal>
-                    </View>
-          )
+        <TouchableOpacity style={styles.cartBtn} onPress={onCartPress}>
+          <>
+            <AntDesign name="shoppingcart" size={24} color="#F29558" />
+            {productInCart ? <View style={styles.badge}>
+              <Text style={styles.badgeText} numberOfLines={1}>{productInCart.QUANTITE}</Text>
+            </View> : null}
+          </>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.productNames}>
+        <Text numberOfLines={2} style={styles.productName}>
+
+          <Text numberOfLines={2} style={styles.productName}> {product.produit_partenaire.NOM}</Text>
+        </Text>
+      </View>
+      {product.produit_partenaire.PRIX ? <Text style={{ color: "#F29558", fontWeight: "bold" }}>{product.produit_partenaire.PRIX.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} Fbu</Text> : null}
+      <Portal>
+        <GestureHandlerRootView style={{ height: isOpen ? '100%' : 0, opacity: isOpen ? 1 : 0, backgroundColor: 'rgba(0, 0, 0, 0)', position: 'absolute', width: '100%', zIndex: 1 }}>
+          <Modalize
+            ref={modalizeRef}
+            adjustToContentHeight
+            handlePosition='inside'
+            modalStyle={{
+              borderTopRightRadius: 25,
+              borderTopLeftRadius: 25,
+              paddingVertical: 20
+            }}
+            handleStyle={{ marginTop: 10 }}
+            scrollViewProps={{
+              keyboardShouldPersistTaps: "handled"
+            }}
+            onClosed={() => {
+              setIsOpen(false)
+              setLoadingForm(true)
+            }}
+          >
+            <AddCart colors={colors} onSizePress={onSizePress} SIZES={SIZES} product={product} loadingForm={loadingForm} onClose={onCloseAddToCart} />
+
+          </Modalize>
+        </GestureHandlerRootView>
+      </Portal>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-          product: {
-                    maxWidth: 200
-          },
-          imageCard: {
-                    borderRadius: 8,
-                    height: "60%",
-                    width: "100%"
-          },
-          image: {
-                    height: "100%",
-                    width: "100%",
-                    borderRadius: 8,
-                    resizeMode: 'contain'
-          },
-          cardLike: {
-                    marginTop: 10,
-                    width: 35,
-                    height: 35,
-                    backgroundColor: "#FBD5DA",
-                    borderRadius: 10,
-                    justifyContent: "center",
-                    alignItems: "center"
-          },
-          cartBtn: {
-                    marginTop: 10,
-                    width: 35,
-                    height: 35,
-                    backgroundColor: "#FBD5DA",
-                    borderRadius: 10,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginLeft: 8
-          },
-          badge: {
-                    minWidth: 25,
-                    minHeight: 20,
-                    paddingHorizontal: 5,
-                    borderRadius: 20,
-                    backgroundColor: COLORS.ecommerceRed,
-                    position: 'absolute',
-                    top: -5,
-                    right: -10,
-                    justifyContent: "center",
-                    alignItems: "center",
-          },
-          badgeText: {
-                    textAlign: 'center',
-                    fontSize: 10,
-                    color: '#FFF',
-                    fontWeight: "bold"
-          },
-          productName: {
-                    color: COLORS.ecommercePrimaryColor,
-                    fontWeight: "400",
-                    fontSize: 13         
-          }
+  product: {
+    maxWidth: 200
+  },
+  imageCard: {
+    borderRadius: 8,
+    height: "60%",
+    width: "100%"
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 8,
+    resizeMode: 'contain'
+  },
+  cardLike: {
+    marginTop: 10,
+    width: 35,
+    height: 35,
+    backgroundColor: "#FBD5DA",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cartBtn: {
+    marginTop: 10,
+    width: 35,
+    height: 35,
+    backgroundColor: "#FBD5DA",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 8
+  },
+  badge: {
+    minWidth: 25,
+    minHeight: 20,
+    paddingHorizontal: 5,
+    borderRadius: 20,
+    backgroundColor: COLORS.ecommerceRed,
+    position: 'absolute',
+    top: -5,
+    right: -10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  badgeText: {
+    textAlign: 'center',
+    fontSize: 10,
+    color: '#FFF',
+    fontWeight: "bold"
+  },
+  productName: {
+    color: COLORS.ecommercePrimaryColor,
+    fontWeight: "400",
+    fontSize: 13
+  }
 })
